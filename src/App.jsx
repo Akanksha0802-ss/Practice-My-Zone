7c0e084be3b979b8e9716ba4f76d11a72565c719
Akanksha0802-ss <akankshasingh91259@gmail.com>
2026-02-17 15:18:36 +0530

    added Student2.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Student1 from './Student1'
+import Student2 from './Student2'
 
 const App = () => {
   return (
     <div>
       <Student1 />
+      <Student2 />
     </div>
   )
 }
---
+++ src/Student2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Student2 = () => {
+  return (
+    <div>
+        <p>Ajay Pratap Singh</p>
+        <p>55</p>
+    </div>
+  )
+}
+
+export default Student2
