1b255f3d2b228b5820717253b672bdb4a8f162b2
Akanksha0802-ss <akankshasingh91259@gmail.com>
2026-02-17 15:09:27 +0530

    reactJs Lecture-25 learning props

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Student1 from './Student1'
+
+const App = () => {
+  return (
+    <div>
+      <Student1 />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Student1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Student1 = () => {
+  return (
+    <div>
+        <p>Akanksha</p>
+        <p>23</p>
+    </div>
+  )
+}
+
+export default Student1
+
+//this is child component
