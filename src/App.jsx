9e561452ee785eb56b992cd1383160bbeac6add6
Akanksha0802-ss <akankshasingh91259@gmail.com>
2026-02-18 14:57:12 +0530

    create Student component using props

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Student from './Student'
 const App = () => {
   return (
     <div>
-      //here, component load with student data feed, this is a props defination
       <Student Myname="vikas" age="18" salary="200000"/>
       <Student Myname="akash" age="28" salary="500000" />
     </div>
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,14 +1,35 @@
+// import React from 'react'
+
+// const Student = ({Myname,age}) => {
+//here object destructuring ,never memory wastage
+//     let obj = {
+//         name: "Akanksha",
+//         age: 24,
+//         address: "Vns"
+//     }
+//     const { name } = obj;
+//     console.log(name);
+//     //console.log(props);
+//     return (
+//     <div>
+//         <p>{Myname}</p>
+//         <p>{age}</p>
+//     </div>
+//   )
+// }
+
+// export default Student
+
+//---------------------------
 import React from 'react'
 
-const Student = (props) => {
-    console.log(props); //props keh andr object hota h
+const Student = ({Myname,age}) => {
     return (
     <div>
-        <p>{props.Myname}</p>
-        <p>{props.age}</p>
+        <p>{Myname}</p>
+        <p>{age}</p>
     </div>
   )
 }
 
 export default Student
-
