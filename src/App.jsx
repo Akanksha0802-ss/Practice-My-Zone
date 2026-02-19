8ff5ddec46cc9d5fd4a097c9593db899e8b898dc
Akanksha0802-ss <akankshasingh91259@gmail.com>
2026-02-19 14:20:27 +0530

    advanced props goes from child to parent tricky

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import Student from './Student'
+// import React from 'react'
+// import Student from './Student'
 
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Student Myname="vikas" age="18" salary="200000"/>
+//       <Student Myname="akash" age="28" salary="500000" />
+//     </div>
+  
+//   )
+// }
+
+// export default App
+
+//---------------------
+import React, { useState } from "react";
+import Child from "./Child";
 
 const App = () => {
+  const [name, setName] = useState("");
   return (
     <div>
-      <Student Myname="vikas" age="18" salary="200000"/>
-      <Student Myname="akash" age="28" salary="500000" />
+      <Child name={name} setName={setName} />
+      <p>value coming from child:{name}</p>
     </div>
-  
   )
 }
 
-export default App
+export default App;
---
+++ src/Child.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Child = (props) => {
+    //console.log(props);
+    return (
+    <div>
+        <input value={props.name} placeholder="enetr name" type="text" onChange={(e) => props.setName(e.target.value)}></input>
+        <p>value inside child:{props.name}</p>
+    </div>
+  )
+}
+
+export default Child
